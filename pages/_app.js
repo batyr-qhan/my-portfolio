f3cdbd2024ec277d2343b47bc9890e18d168b0f1
batyrqh <batyrqh@protonmail.com>
2022-11-16 15:15:57 +0300

    add: animation page open and refresh

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <motion.div
+      key={router.route}
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+      }}
+      transition={{
+        duration: 0.8,
+      }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </motion.div>
   );
 }
 
